d04c19b38252a2ac31d61d3f8e2477f68170a153
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-09-03 21:15:05 -0500

    Navbar completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { FiShoppingBag } from "react-icons/fi";
 
 const Navbar = () => {
   return (
     <div className="w-full h-[80px] bg-white border-b">
-      <div className="max-w-[1024px] m-auto">
-        <h1>Shop</h1>
+      <div className="md:max-w-[1024px] max-w-[600px] mx-auto w-full h-full flex justify-between items-center p-6">
+        <h1 className="font-moderustic font-bold text-2xl">Shop</h1>
+        <FiShoppingBag size={24} />
       </div>
     </div>
   );
